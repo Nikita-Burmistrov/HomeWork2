f02abd90ce4853c6a6457f8e1d94e0412b52b92e
burmi@DESKTOP-EE7A2UG <burmistrov.03@mail.ru>
2022-08-01 17:34:39 +0300

    HomeWork2

---
+++ src/HomeEx1.java
@@ -0,0 +1,19 @@
+public class HomeEx1 {
+    public static void main(String[] args) {
+        int d=(439);
+        switch (d){
+            case 439:
+                System.out.println("Это число 439");
+                break;
+            case 153:
+                System.out.println("Это число 153");
+                break;
+            case 450:
+                System.out.println("Это число 450");
+                break;
+            default:
+                System.out.println("Увы не какое число не подходит");
+
+        }
+    }
+}
---
+++ src/HomeEx2.java
@@ -0,0 +1,15 @@
+public class HomeEx2 {
+    public static void main(String[] args) {
+        int b=1;
+        int a=0;
+        if (a > b) {
+                System.out.println("a больше b");
+        }else if (a < b) {
+            System.out.println("b больше a");
+        }else if (a == b) {
+            System.out.println("а и b схожи");
+        }else {
+            System.out.println("Вероятно где-то допущенна ошибка!");
+        }
+    }
+}
